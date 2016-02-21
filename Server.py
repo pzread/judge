76af0938fa6cc0b753f8acd5445065fc01fec1e2
pzread <netfirewall@gmail.com>
2016-02-21 12:24:02 +0800

    Change for night test version

--- Server.py
+++ Server.py
@@ -4,7 +4,6 @@ from collections import deque
 from tornado import gen, concurrent
 from tornado.ioloop import IOLoop, PollIOLoop
 from tornado.web import Application, RequestHandler
-from tornado.websocket import WebSocketHandler
 import PyExt
 from StdChal import StdChal
 import Config
@@ -15,99 +14,18 @@ class UVIOLoop(PollIOLoop):
         super().initialize(impl = PyExt.UvPoll(), **kwargs)
 
 
-class JudgeHandler(WebSocketHandler): 
-    chal_running_count = 0
-    chal_queue = deque()
-
-    @gen.coroutine
-    def start_chal(obj, ws):
-        chal_id = obj['chal_id']
-        code_path = '/srv/nfs' + obj['code_path'][4:]
-        test_list = obj['testl']
-        res_path = '/srv/nfs' + obj['res_path'][4:]
-
-        test_paramlist = list()
-        comp_type = test_list[0]['comp_type']
-        assert(comp_type in ['g++', 'clang++', 'makefile', 'python3'])
-
-        for test in test_list:
-            assert(test['comp_type'] == comp_type)
-            assert(test['check_type'] == 'diff')
-            test_idx = test['test_idx']
-            memlimit = test['memlimit']
-            timelimit = test['timelimit']
-            data_ids = test['metadata']['data']
-            for data_id in data_ids:
-                test_paramlist.append({
-                    'in': res_path + '/testdata/%d.in'%data_id,
-                    'ans': res_path + '/testdata/%d.out'%data_id,
-                    'timelimit': timelimit,
-                    'memlimit': memlimit,
-                })
-
-        chal = StdChal(chal_id, code_path, comp_type, res_path, test_paramlist)
-        result_list = yield chal.start()
-
-        idx = 0
-        for test in test_list:
-            test_idx = test['test_idx']
-            data_ids = test['metadata']['data']
-            total_runtime = 0
-            total_mem = 0
-            total_status = 0
-            for data_id in data_ids:
-                runtime, peakmem, status = result_list[idx]
-                total_runtime += runtime
-                total_mem += peakmem
-                total_status = max(total_status, status)
-                idx += 1
-
-            ws.write_message(json.dumps({
-                'chal_id': chal_id,
-                'test_idx': test_idx,
-                'state': total_status,
-                'runtime': total_runtime,
-                'memory': total_mem,
-            }))
-
-        JudgeHandler.chal_running_count -= 1
-        JudgeHandler.emit_chal()
-
-    def emit_chal(obj=None, ws=None):
-        if obj is not None:
-            JudgeHandler.chal_queue.append((obj, ws))
-
-        while len(JudgeHandler.chal_queue) > 0 \
-            and JudgeHandler.chal_running_count < Config.TASK_MAXCONCURRENT:
-            chal = JudgeHandler.chal_queue.popleft()
-            JudgeHandler.chal_running_count += 1
-            IOLoop.instance().add_callback(JudgeHandler.start_chal, *chal)
-
-    def open(self): 
-        pass 
-
-    def on_message(self, msg): 
-        obj = json.loads(msg, 'utf-8')
-        JudgeHandler.emit_chal(obj, self)
-
-    def on_close(self): 
-        pass
-
-
-@concurrent.return_future
-def test(callback):
-    def _done_cb(task_id, stat):
-        print(stat)
-        callback()
-
-    task_id = PyExt.create_task('/usr/bin/python3.4',
-        ['-m', 'py_compile', '/test.py'],
-        ['HOME=/'],
-        0, 1, 2,
-        '/', 'container/standard',
-        10000, 10000, 100000000, 256 * 1024 * 1024,
-        PyExt.RESTRICT_LEVEL_HIGH)
-    PyExt.start_task(task_id, _done_cb)
+@gen.coroutine
+def test():
+    chal = StdChal(1, 'lib/test.cpp', 'g++', 'lib', [
+        {
+            'in': 'lib/in.txt',
+            'ans': 'lib/out.txt',
+            'timelimit': 2000,
+            'memlimit': 256 * 1024 * 1024,
+        }    
+    ])
+    result_list = yield chal.start()
+    print(result_list)
 
 
 def main():
@@ -115,12 +33,7 @@ def main():
     StdChal.init()
     IOLoop.configure(UVIOLoop)
 
-    app = Application([
-        (r'/judge', JudgeHandler),
-    ])
-    app.listen(2501)
-
-    #IOLoop.instance().add_callback(test)
+    IOLoop.instance().add_callback(test)
 
     IOLoop.instance().start()
 
--- build-container.sh
+++ build-container.sh
@@ -15,8 +15,6 @@ cp /usr/bin/as $1/usr/bin/as
 cp /usr/bin/ld $1/usr/bin/ld
 cp /usr/bin/make $1/usr/bin/make
 cp /usr/bin/python3.4 $1/usr/bin/python3.4
-cp /usr/lib/libopcodes-2.25.1-system.so $1/usr/lib/libopcodes-2.25.1-system.so
-cp /usr/lib/libbfd-2.25.1-system.so $1/usr/lib/libbfd-2.25.1-system.so
 cp -a /usr/lib/x86_64-linux-gnu $1/usr/lib/x86_64-linux-gnu
 cp -a /usr/lib/gcc $1/usr/lib/gcc
 cp -a /usr/lib/llvm-3.6 $1/usr/lib/llvm-3.6
