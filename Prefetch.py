9896d3a0550dcf162b3cc9bf419292fa03fd9d60
pzread <netfirewall@gmail.com>
2016-02-23 00:10:34 +0800

    Fix sphinx problem

--- Prefetch.py
+++ Prefetch.py
@@ -4,12 +4,20 @@
 import sys
 import os
 
-for filepath in sys.argv[1:]:
-    infile_fd = os.open(filepath, os.O_RDONLY)
-    infile_size = os.fstat(infile_fd).st_size
-    for i in range(0, infile_size, 65536):
-        os.lseek(infile_fd, i, os.SEEK_SET)
-        os.read(infile_fd, 65536)
-    os.close(infile_fd)
 
-print('1')
+def main():
+    '''Main function.'''
+
+    for filepath in sys.argv[1:]:
+        infile_fd = os.open(filepath, os.O_RDONLY)
+        infile_size = os.fstat(infile_fd).st_size
+        for i in range(0, infile_size, 65536):
+            os.lseek(infile_fd, i, os.SEEK_SET)
+            os.read(infile_fd, 65536)
+        os.close(infile_fd)
+
+    print('1')
+
+
+if __name__ == '__main__':
+    main()
